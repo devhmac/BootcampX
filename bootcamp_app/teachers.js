01cc62df05e827d1826b214cc60b7bd29e0e7401
Devin MacGillivray <devhmac@gmail.com>
2021-04-01 00:36:12 +0000

    teacher query on db

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort 
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+  .then(res => {
+    // console.log(res.rows)
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`)
+    })
+  })
+  .catch(err => console.log('query error ', err.stack));
